6acf07a1758cc29cabe394a8d3d167703351c159
AdachiAtsushi <atsuk0r05296@gmail.com>
2022-06-16 11:04:38 +0900

    #6 タスク登録画面の作成
    登録画面の初期表示時の処理を作成した。
    登録機能を新規作成した。
    テーブル名「Task」の定義を変更した。
    マスタテーブル名「mst_priority」の定義追加を行った。

--- taskManagement/src/main/resources/schema.sql
+++ taskManagement/src/main/resources/schema.sql
@@ -4,13 +4,13 @@ CREATE TABLE task (
     comment VARCHAR(100) NOT NULL,
     priority CHAR(1) NOT NULL,
     start_time DATETIME NOT NULL,
-    end_time DATETIME,
+    end_time DATETIME NOT NULL,
     finish_flg BOOLEAN,
     PRIMARY KEY(id)
 );
 
--- FIXME àÍóóâÊñ ÇÃëÃçŸÇïœçXÇ∑ÇÈç€Ç…íËã`Ç∑ÇÈ
--- CREATE mst_priority (
---    priority CHAR(1) NOT NULL,
---    priority_value CHAR(1) NOT NULL
--- );
+CREATE TABLE mst_priority (
+  priority CHAR(1) NOT NULL,
+  priority_text CHAR(1) NOT NULL,
+  PRIMARY KEY(priority)
+);
