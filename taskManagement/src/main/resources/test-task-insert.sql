46b70327eab056f309e2eb512773c5003fe7a758
AdachiAtsushi <atsuk0r05296@gmail.com>
2022-07-14 23:46:57 +0900

    #11 各種機能のテストクラス作成
    設定ファイルの新規作成、修正を行った。

---
+++ taskManagement/src/main/resources/test-task-insert.sql
@@ -0,0 +1,6 @@
+-- テーブル名「タスク」 
+INSERT INTO task (id, title, comment, priority, start_time, end_time, status_id) VALUES (100, '筋トレ', '腕立て伏せをする', '1', '2022-06-01 12:00:00', '2022-06-02 12:00:00', '1');
+INSERT INTO task (id, title, comment, priority, start_time, end_time, status_id) VALUES (101, '読書', '自己啓発本を読む', '1', '2022-06-01 12:00:00', '2022-06-02 12:00:00', '1');
+INSERT INTO task (id, title, comment, priority, start_time, end_time, status_id) VALUES (102, '事務処理', '領収書を整理する', '2', '2022-06-01 12:00:00', '2022-06-02 12:00:00', '1');
+INSERT INTO task (id, title, comment, priority, start_time, end_time, status_id) VALUES (103, '買い物(日用品)', 'ティッシュを買う', '2', '2022-06-01 12:00:00', '2022-06-02 12:00:00', '1');
+INSERT INTO task (id, title, comment, priority, start_time, end_time, status_id) VALUES (104, '掃除', 'リビングを掃除する', '3', '2022-06-01 12:00:00', '2022-06-02 12:00:00', '1');
